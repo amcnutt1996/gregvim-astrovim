9b698053cac2d017d7cec05bea39e82993ec4516
Gregory McNutt <amcnutt1996@gmail.com>
2026-06-01 01:26:04 -0400

    removed swiftkit plugin lua usage in polish

--- lua/polish.lua
+++ lua/polish.lua
@@ -4,8 +4,5 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Enable sourcekit LSP for swift language
-vim.lsp.enable "sourcekit"
-
 -- Disable relative line numbers in editor
 vim.o.relativenumber = false
